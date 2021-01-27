951afc0821783dac188dec21a184d7831ffb229a
RakibulIslamDigonto <rakibdigonto11@gmail.com>
2021-01-28 00:50:45 +0600

    forthbranch created

--- hello.py
+++ hello.py
@@ -4,12 +4,8 @@ print('hello this is third fail')
 def thirBranch():
     print('this is third branch file')
 
-def another():
-    print('another file from third')
+def forthb():
+    print('forth branch massage')
 
-def hello():
-    print('this is sec branch file')
 
-def secHello():
-    print('another sec branch function')
 
